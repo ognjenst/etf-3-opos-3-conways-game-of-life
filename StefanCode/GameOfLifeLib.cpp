b43f1a96fffcb2166d94fdd176039c391ca442c9
Ognjen Stefanovic <stefanovic.ognjen98@gmail.com>
2021-01-07 00:37:51 +0100

    Saving image done

--- StefanCode/GameOfLifeLib.cpp
+++ StefanCode/GameOfLifeLib.cpp
@@ -5,6 +5,15 @@
 #include <CL/cl.h>
 #include <string>
 #include <thread>
+#include <stdio.h>
+
+const int BYTES_PER_PIXEL = 3; /// red, green, & blue
+const int FILE_HEADER_SIZE = 14;
+const int INFO_HEADER_SIZE = 40;
+
+void generateBitmapImage(unsigned char* image, int height, int width, char* imageFileName);
+unsigned char* createBitmapFileHeader(int height, int stride);
+unsigned char* createBitmapInfoHeader(int height, int width);
 
 
 bool isPrime(int num) {
@@ -91,15 +100,17 @@ void LoadCustomMatrix(unsigned char*& buffer, int &width, int &height)
 	buffer = image;
 }
 
-void RunTheGame(unsigned char*& buffer, int width, int height, int numberOfIterations, int startIteration = 0, int numberOfWorkItems = 8)
+void RunTheGame(unsigned char*& buffer, const int width, const int height, int numberOfIterations, int startIteration = 0, int numberOfWorkItems = 8)
 {
 	int imageSize = width * height;
 	unsigned char *buffer2, *buffer3, *buffer1;
 	buffer1 = new unsigned char[(size_t)width * height];
 	buffer2 = new unsigned char[(size_t)width * height];
 	buffer3 = new unsigned char[(size_t)width * height];
+	bool repetition = false;
 	for (int i = 0; i < numberOfIterations; i++)
 	{
+		if (repetition) break;
 		cl_mem d_a;
 		cl_platform_id cpPlatform;        // OpenCL platform
 		cl_device_id device_id;           // device ID
@@ -207,23 +218,30 @@ void RunTheGame(unsigned char*& buffer, int width, int height, int numberOfItera
 		memcpy(buffer1, buffer, (size_t)(height * width));
 		if (!memcmp(buffer3, buffer, (size_t)(height * width)))
 		{
+			unsigned char* image = new unsigned char[height * width * BYTES_PER_PIXEL];
+			char* imageFileName = (char*)"bitmapImage.bmp";
 
-			std::cout << "OP";
-			//unsigned char bmpfile_header[54];
-			//FILE* fp = fopen("white.bmp", "wb");
-			//fread(bmpfile_header, sizeof(unsigned char), 54, fp); // read the 54-byte
-			//unsigned char data[] = {'0'};
-			//fseek(fp, 54, SEEK_SET);
-			//for (int i = 0; i < 256; i++) {
-			//	for (int j = 0; j < 256; j++) {
-			//		/*buffer[i * 256 + j];*/
-			//		fwrite(data, 1, 1, fp);
-			//		fwrite(data, 1, 1, fp);
-			//		fwrite(data, 1, 1, fp);
-			//	}
-			//}
+			int a, b;
+			for (a = 0; a < height; a++) {
+				for (b = 0; b < width; b++) {
+					if (buffer[a * width + b] == 0)
+					{
+						*(image + a * width * BYTES_PER_PIXEL + b * BYTES_PER_PIXEL + 2) = (unsigned char)(255);             ///red
+						*(image + a * width * BYTES_PER_PIXEL + b * BYTES_PER_PIXEL + 1) = (unsigned char)(0);              ///green
+						*(image + a * width * BYTES_PER_PIXEL + b * BYTES_PER_PIXEL + 0) = (unsigned char)(0); ///blue
+					}
+					else
+					{
+						*(image + a * width * BYTES_PER_PIXEL + b * BYTES_PER_PIXEL + 2) = (unsigned char)(255);             ///red
+						*(image + a * width * BYTES_PER_PIXEL + b * BYTES_PER_PIXEL + 1) = (unsigned char)(255);              ///green
+						*(image + a * width * BYTES_PER_PIXEL + b * BYTES_PER_PIXEL + 0) = (unsigned char)(255); ///blue
+					}
+				}
+			}
 
-			//fclose(fp);
+			generateBitmapImage((unsigned char*)image, height, width, imageFileName);
+			repetition = true;
+			
 		}
 
 		if (i >= startIteration)
@@ -260,4 +278,85 @@ void RunGameFromCustomMatrix(int numberOfIterations, int startIteration)
 	else if (width * height % 2 == 0) devisor = 2;
 
 	RunTheGame(buffer, width, height, numberOfIterations, startIteration,  5);
+}
+
+
+// BMP Generation
+void generateBitmapImage(unsigned char* image, int height, int width, char* imageFileName)
+{
+	int widthInBytes = width * BYTES_PER_PIXEL;
+
+	unsigned char padding[3] = { 0, 0, 0 };
+	int paddingSize = (4 - (widthInBytes) % 4) % 4;
+
+	int stride = (widthInBytes)+paddingSize;
+
+	FILE* imageFile = fopen(imageFileName, "wb");
+
+	unsigned char* fileHeader = createBitmapFileHeader(height, stride);
+	fwrite(fileHeader, 1, FILE_HEADER_SIZE, imageFile);
+
+	unsigned char* infoHeader = createBitmapInfoHeader(height, width);
+	fwrite(infoHeader, 1, INFO_HEADER_SIZE, imageFile);
+
+	int i;
+	for (i = 0; i < height; i++) {
+		fwrite(image + (i * widthInBytes), BYTES_PER_PIXEL, width, imageFile);
+		fwrite(padding, 1, paddingSize, imageFile);
+	}
+
+	fclose(imageFile);
+}
+
+unsigned char* createBitmapFileHeader(int height, int stride)
+{
+	int fileSize = FILE_HEADER_SIZE + INFO_HEADER_SIZE + (stride * height);
+
+	static unsigned char fileHeader[] = {
+		0,0,     /// signature
+		0,0,0,0, /// image file size in bytes
+		0,0,0,0, /// reserved
+		0,0,0,0, /// start of pixel array
+	};
+
+	fileHeader[0] = (unsigned char)('B');
+	fileHeader[1] = (unsigned char)('M');
+	fileHeader[2] = (unsigned char)(fileSize);
+	fileHeader[3] = (unsigned char)(fileSize >> 8);
+	fileHeader[4] = (unsigned char)(fileSize >> 16);
+	fileHeader[5] = (unsigned char)(fileSize >> 24);
+	fileHeader[10] = (unsigned char)(FILE_HEADER_SIZE + INFO_HEADER_SIZE);
+
+	return fileHeader;
+}
+
+unsigned char* createBitmapInfoHeader(int height, int width)
+{
+	static unsigned char infoHeader[] = {
+		0,0,0,0, /// header size
+		0,0,0,0, /// image width
+		0,0,0,0, /// image height
+		0,0,     /// number of color planes
+		0,0,     /// bits per pixel
+		0,0,0,0, /// compression
+		0,0,0,0, /// image size
+		0,0,0,0, /// horizontal resolution
+		0,0,0,0, /// vertical resolution
+		0,0,0,0, /// colors in color table
+		0,0,0,0, /// important color count
+	};
+
+	infoHeader[0] = (unsigned char)(INFO_HEADER_SIZE);
+	infoHeader[4] = (unsigned char)(width);
+	infoHeader[5] = (unsigned char)(width >> 8);
+	infoHeader[6] = (unsigned char)(width >> 16);
+	infoHeader[7] = (unsigned char)(width >> 24);
+	infoHeader[8] = (unsigned char)(height);
+	infoHeader[9] = (unsigned char)(height >> 8);
+	infoHeader[10] = (unsigned char)(height >> 16);
+	infoHeader[11] = (unsigned char)(height >> 24);
+	infoHeader[12] = (unsigned char)(1);
+	infoHeader[14] = (unsigned char)(BYTES_PER_PIXEL * 8);
+
+	return infoHeader;
 }
